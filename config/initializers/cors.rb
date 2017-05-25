a667daca8b70ee0c212caf4ce4f5ae4eb8ea0119
Mark De May <markdemay@TirNaNog.PK5001Z>
2017-05-25 14:22:01 -0600

    Fixed cors for heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,6 +7,22 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    origins 'https://art-from-light.herokuapp.com/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
+    origins 'http://art-from-light.herokuapp.com/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
     origins 'localhost:3001'
 
     resource '*',
