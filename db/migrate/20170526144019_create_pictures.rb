12a69e66799a91088728603b2899991f78c40422
Mark De May <markdemay@TirNaNog.PK5001Z>
2017-05-26 09:22:36 -0600

    Pictures table

---
+++ db/migrate/20170526144019_create_pictures.rb
@@ -0,0 +1,11 @@
+class CreatePictures < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pictures do |t|
+      t.string :title
+      t.string :description
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create(
+  name: "Mark",
+  password: 'Mark',
+  address: Faker::Address.city + ', ' + Faker::Address.state_abbr,
+  profile_url: 'http://i.imgur.com/GqF6FMvt.jpg'
+)
+
+3.times do
+  Picture.create(
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.sentence,
+    user_id: 1
+  )
+end
+
+
 3.times do
   User.create(
     name: Faker::Name.name,
@@ -15,12 +31,6 @@
   )
 end
 
-User.create(
-  name: "Mark",
-  password: 'Mark',
-  address: Faker::Address.city + ', ' + Faker::Address.state_abbr,
-  profile_url: 'http://i.imgur.com/GqF6FMvt.jpg'
-)
 
 
 User.create(
