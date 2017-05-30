b031c92b664b094638d328d0ba853b5f8a59069e
Mark De May <markdemay@TirNaNog.PK5001Z>
2017-05-30 14:48:25 -0600

    Return correct user data from update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
+      set_user()
       render json: @user
     else
       render json: @user.errors, status: :unprocessable_entity
